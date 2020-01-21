ee818ae85f8c9f47aaae7950dca0bc788800b184
jusupbekovaa <jusupbekova7@gmail.com>
2020-01-21 20:37:25 +0600

    Добавьте файлы проекта.

---
+++ App2-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace App2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int i;
+            Console.WriteLine("Введите 10 чисел: ");
+            for (i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i=0; i<array.Length;i++)
+            {
+                if(array[i]<=0)
+                {
+                    array[i] = 1;
+                }
+                Console.WriteLine($"X[{i}] = {array[i]}");
+            }
+        }
+    }
+}
